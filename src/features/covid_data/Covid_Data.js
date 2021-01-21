96dd4961cffc15fe49a2aa91de7a0cfdcde392a3
Alexenfeu <alexissarlat@gmail.com>
2021-01-21 15:07:01 +0100

    covid ok

--- src/features/covid_data/Covid_Data.js
+++ src/features/covid_data/Covid_Data.js
@@ -1,22 +1,46 @@
-import React, {useRef, useEffect} from "react";
-import {Runtime, Inspector} from "@observablehq/runtime";
-import notebook from "7a24795d54ccd03f";
+import React from "react";
 
-function Covid_Cases() {
-    const ref = useRef();
-  
-    useEffect(() => {
-      (new Runtime).module(notebook, name => {
-        if (name === "chart") return Inspector.into(ref.current.querySelector(".chart"))();
-      });
-    }, []);
-  
-    return (
-      <div className="Notebook" ref={ref}>
-        <div className="chart"></div>
-        <p>Credit: <a href="https://observablehq.com/d/7a24795d54ccd03f">Week 1: Cupcake Barchart by alexenfeu</a></p>
-      </div>
-    );
-  }
-  
-  export default Covid_Cases;
+import dataRaw from "../../covid_data.json";
+
+import {Box, Text, Meter, Chart, Select} from "grommet";
+
+import {LineChart, ReferenceLine, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line, Area, AreaChart, ComposedChart, BarChart, Bar, Cell} from "recharts";
+
+
+
+const datas = new Array();
+dataRaw.map(item => {
+    datas.push(item)
+});
+
+console.log(datas);
+
+datas.shift();
+datas.splice(8, datas.length - 8);
+
+const Covid_Cases = () => {
+    return(
+        <Box fill align="center" justify="center">
+            <Text>Top 8 Cases, Deaths and Recoveries By Country</Text>
+            <BarChart width={1000} height={300} data={datas} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+              <CartesianGrid strokeDasharray="3 3"/>
+              <XAxis dataKey="Location"/>
+              <YAxis/>
+              <Tooltip/>
+              <Legend />
+              <Bar dataKey="Cases" fill="#067AE3" />
+              <Bar dataKey="Deaths" fill="#E31D06" />
+              <Bar dataKey="Recoveries" fill="#05AC16" />
+            </BarChart>
+        </Box>
+    )}
+
+export default Covid_Cases;
+
+
+
+
+
+
+
+
